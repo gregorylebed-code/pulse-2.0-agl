05b1263d2c56a94b3580db6699545ee8a4e4a426
gregorylebed-code <gregorylebed@gmail.com>
2026-04-10 09:05:21 -0400

    Fix syntax error after demographics removal

--- src/components/StudentDetailView.tsx
+++ src/components/StudentDetailView.tsx
@@ -2123,7 +2123,6 @@ export default function StudentDetailView({
             className="w-full px-3 py-1.5 bg-slate-50 border border-slate-100 rounded-lg text-xs font-medium focus:outline-none focus:border-sage"
           />
         </div>
-        </div>
       </div>
 
       </>}{/* end parents tab */}
